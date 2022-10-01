688fad42c3d0506bb7d0634481878067c5fdfdc2
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-10-01 16:07:02 +0100

    write while loop to calculate input num. of primes

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    list.append(num)
+    while(len(list) < number_of_primes):
+        counter = 0
+        num+=1
+        for i in range(2,int(num/2)+1):
+            if(num % i == 0):
+                counter+=1
+                break
+        if(counter ==0):
+            list.append(num)
     return list
